c375bbadd5e86c9bc6df1dffbb6158ab08308419
alexanderroca <alexander.roca@students.salle.url.edu>
2018-12-05 11:08:36 +0100

    doc: migracio del RadixSort i SelectionSort de test a la carpeta de sort

--- src/Sort/SelectionSort.java
+++ src/Sort/SelectionSort.java
@@ -1,5 +1,7 @@
 package Sort;
 
+import GsonObjects.Post;
+
 /**
  *Classe que defineix el mètode d'ordenació SelectionSort
  *
@@ -14,7 +16,7 @@ public class SelectionSort {
         for(int i = 0; i < array.length - 1; i++){
 
             int index = i;
-            for (int j = index + 1; j < array.length; j++){
+            for (int j = i + 1; j < array.length; j++){
                 if(array[j] < array[index])
                     index = j;
 
@@ -27,4 +29,27 @@ public class SelectionSort {
         return array;
     }
 
+
+    /**
+     * Funcio que ordena els posts segons la seva data de publicacio
+     * @param array : array de Post
+     * @return array de Post ordenat segons la seva data de publicacio
+     */
+    public Post[] selectionSortPosts(Post[] array){
+        for(int i = 0; i < array.length - 1; i++){
+
+            int index = i;
+            for (int j = i + 1; j < array.length; j++){
+
+                if(array[j].getPublished() > array[index].getPublished())
+                    index = j;
+
+                Post aux = array[i];
+                array[i] = array[index];
+                array[index] = aux;
+            }   //for
+        }   //for
+
+        return array;
+    }
 }
